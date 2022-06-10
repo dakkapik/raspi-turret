aa39c7ceeeae6a53b9b80c24a92ba5c21f6bd9d3
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-06-09 21:27:48 -0400

    turret mouse control

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let piblaster
 if(process.platform === 'linux') piblaster = require("pi-blaster.js")
+const ratio = 180 / 605
 
 const { io } = require("socket.io-client")
-
 
 const socket = io ("http://192.168.0.210:5000")
 
@@ -16,4 +16,9 @@ socket.on("turret-pos", ( pos ) => {
     piblaster.setPwm(4, pos.x)
     // pin 17 is roll
     // piblaster.setPwm(17)
+})
+
+socket.on("mouse-pos", ( pos )=> {
+    piblaster.setPwm(4, (pos.x * ratio) / 1000 + 0.06)
+    piblaster.setPwm(17, (pos.y * ratio) / 1000 + 0.06)
 })
